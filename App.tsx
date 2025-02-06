a73cb6ab3f973da6b67c899f992dfe3d977d49a5
fredo09 <fred_15@outlook.com>
2025-02-05 20:46:28 -0600

    init commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Components
+import { FloatingButton} from './components';
+
 export default function App() {
+  const [ count, setCount ] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Counter App</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      <FloatingButton
+        label="+1"
+        position="right"
+        onPress={() => setCount(count + 1)}
+      />
+
+      <FloatingButton
+        label="Reset"
+        onLongPress={() => setCount(0)}
+      />
+
+      <FloatingButton
+        label="-1"
+        position="left"
+        onPress={() => setCount(count - 1)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textHuge: {
+    fontSize: 120,
+    fontWeight: "100",
   },
 });
---
+++ components/FloatingButton/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Text, StyleSheet, Pressable } from "react-native";
+
+interface Props  {
+    label: string;
+    position?: 'left' | 'right' | 'center';
+    //Methods
+    onPress?: () => void;
+    onLongPress?: () => void;
+}
+
+export const FloatingButton = ({ onPress, onLongPress, label, position = 'center' }: Props) => {
+    return (
+        <Pressable
+        style={({ pressed }) => [
+            styles.floatingButton,
+            position === 'right' && styles.positionRight,
+            position === 'left' && styles.positionLeft,
+            position === 'center' && styles.positionCenter,
+            pressed ? { opacity: 0.5 } : { opacity: 1 },
+        ]}
+        onPress={onPress}
+        onLongPress={onLongPress}
+        >
+            <Text style={styles.colorCounterBtn}>{label}</Text>
+        </Pressable>
+    );
+};
+
+//* Styles
+const styles = StyleSheet.create({
+    floatingButton: {
+        position: "absolute",
+        bottom: 20,
+        backgroundColor: "#65558f",
+        padding: 20,
+        borderRadius: 15,
+        marginBottom: 20,
+        shadowOpacity: 0.3,
+        elevation: 3,
+    },
+    colorCounterBtn: {
+        color: "#fff",
+        fontSize: 20,
+    },
+    positionRight: {
+        right: 20,
+    },
+    positionLeft: {
+        left: 20,
+    },
+    positionCenter: {
+        alignItems: "center",
+    },
+});
